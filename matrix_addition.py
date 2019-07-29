4acc721483928094a8f90a368920a2498a7f94ac
deepak <deepaktiwari221299@gmail.com>
2019-07-29 23:18:07 +0530

    add_matrix

---
+++ matrix_addition.py
@@ -0,0 +1,10 @@
+
+from numpy import *
+
+matrix_1=matrix('1 2 3 ; 2 3 4 ; 5 6 7')  ### its 3x3 matrix
+matrix_2=matrix('2 3 4 ; 8 5 4 ; 2 3 6')  ### its 3x3 matrix
+
+add_matrix=matrix_1+matrix_2
+
+print(f"{matrix_1} \n    + \n {matrix_2} \n    = \n {add_matrix}")
+
